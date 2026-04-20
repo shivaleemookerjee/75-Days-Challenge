1b2472119279fe6bdf7212fd98ec1220d77f2180
shivaleemookerjee <shivalee.mookerjee175@gmail.com>
2026-04-20 10:23:31 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,3 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
-
-// 🔔 BACKGROUND PUSH
-self.addEventListener('push', function(event) {
-  const data = event.data.json();
-
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: "https://cdn-icons-png.flaticon.com/512/744/744465.png"
-  });
-});
